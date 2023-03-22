e97e880f1ddb67b6497a7536b857d7a207a3c72d
Maciej <mac.lipiec@icloud.com>
2023-03-22 20:59:37 +0100

    Test

---
+++ NasaViewer/Services/DoNotGitIgnore.swift
@@ -0,0 +1,8 @@
+//
+//  DoNotGitIgnore.swift
+//  NasaViewer
+//
+//  Created by Maciej Lipiec on 2023-03-22.
+//
+
+import Foundation
---
+++ NasaViewer/Services/PrivateData.swift
@@ -0,0 +1,10 @@
+//
+//  PrivateData.swift
+//  NasaViewer
+//
+//  Created by Maciej Lipiec on 2023-03-22.
+//
+
+import Foundation
+
+let NASA_API_KEY = "Test"
